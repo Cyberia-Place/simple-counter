7802713b205dc1b6d64e90852590b5566283e558
Cyberia-Place <eliasberto2@gmail.com>
2021-05-18 01:19:55 +0000

    casi terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,56 @@
-import React, { useState } from "react";
-import { Clock } from "./clock.js";
+import React, { useState, useEffect } from "react";
 
 //create your first component
-export function Home() {
-	let [segundos, setSegundos] = useState(0);
-	// console.log(segundos);
-	setInterval(() => {
-		setSegundos(segundos + 1);
-		console.log(segundos);
-	}, 1000);
+export const Clock = () => {
+	const [seconds, setSeconds] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSeconds(seconds => seconds + 1);
+		}, 1000);
+	}, []);
+
+	const allNumbers = [
+		Math.floor(seconds / 100000),
+		Math.floor(seconds / 10000),
+		Math.floor(seconds / 1000),
+		Math.floor(seconds / 100),
+		Math.floor(seconds / 10),
+		Math.floor(seconds)
+	];
+
+	const horario = allNumbers.map((number, index) => {
+		return (
+			<div className="mediumdiv mx-3" key={index}>
+				<div className="card">
+					<div className="card-body text-center display-1">
+						{number}
+					</div>
+				</div>
+			</div>
+		);
+	});
+
 	return (
-		<div>
-			<Clock segundos={...segundos} />
+		<div className="bigdiv px-4 py-3 d-flex justify-content-center">
+			<div className="mediumdiv mx-3" style={{ width: "10rem" }}>
+				<div className="card">
+					<div className="card-body text-center display-1">
+						<i className="far fa-clock text-white align-self-center"></i>
+					</div>
+				</div>
+			</div>
+			{horario}
 		</div>
 	);
-}
+};
+
+// {
+// 	const [seconds, setseconds] = useState(0);
+// 	useEffect(() => {
+// 		setInterval(() => {
+// 			setseconds(seconds + 1);
+// 		}, 1000);
+// 	});
+
+// };
